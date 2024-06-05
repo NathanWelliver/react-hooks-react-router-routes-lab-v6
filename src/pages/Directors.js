e9edac883b7f391446cabc8d84d92db9d3928aa7
Nathan Welliver <nwelliver.564@gmail.com>
2024-06-05 10:31:35 -0400

    complete lab

--- src/pages/Directors.js
+++ src/pages/Directors.js
@@ -1,13 +1,33 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
 
 function Directors() {
+  const [directorss, setDirectorss] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:4000/directors")
+    .then(r => r.json())
+    .then(data => setDirectorss(data))
+    .catch((error) => console.log(error))
+  }, []);
+
+  const directorList = directorss.map((director) => {
+    return (
+      <article key={director.id}>
+        <h2>{director.name}</h2>
+        <li>{director.movies}</li>
+      </article>
+    )
+  })
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar/>
       </header>
       <main>
-        {/* Director info here! */}
+        <h1>Directors Page</h1>
+        {directorList}
       </main>
     </>
   );
